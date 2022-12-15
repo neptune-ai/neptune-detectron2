d604e9ea8961dd0bd3b3c01cb47e1eab85244962
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2022-12-15 23:38:33 +0100

    make custom_run_id a local variable

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -7,19 +7,19 @@ from detectron2.checkpoint import DetectionCheckpointer
 from src.neptune_detectron2 import NeptuneHook
 from tests.utils import get_images
 
-CUSTOM_RUN_ID = str(uuid4())
-
 
 def test_e2e(cfg, trainer):
+    custom_run_id = str(uuid4())
+
     get_images()
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
     DetectionCheckpointer(trainer.model).load(cfg.MODEL.WEIGHTS)
     trainer.resume_or_load(resume=False)
 
-    trainer.register_hooks([NeptuneHook(log_checkpoints=True, log_model=True, custom_run_id=CUSTOM_RUN_ID)])
+    trainer.register_hooks([NeptuneHook(log_checkpoints=True, log_model=True, custom_run_id=custom_run_id)])
     trainer.train()
 
-    npt_run = neptune.init_run(custom_run_id=CUSTOM_RUN_ID)
+    npt_run = neptune.init_run(custom_run_id=custom_run_id)
 
     assert npt_run.exists("training/config")
 
